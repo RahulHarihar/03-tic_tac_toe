c193b5bc1e8a6b5f5c7babae63b08ead0761aef6
unknown <rahulharihar71@gmail.com>
2024-09-10 17:18:41 +0530

    added logic for the game

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,24 +4,76 @@ import { circle_img, cross_img } from '/src/utils/index.js';
 
 let data = ["", "", "", "", "", "", "", "", "",];
 
-const Board = () => {
+const Board = ({titleRef}) => {
 
   let [count, setCount] = useState(0);
   let[lock, setLock] = useState(false);
+
+  
 
   const toggle = (e, num) => {
     if (lock || data[num]) {
       return;
     }
     if (count % 2 === 0) {
-      e.target.innerHTML = `<img src='${cross_img}' alt="X">`; 
+      e.target.innerHTML = `<img src='${cross_img}' alt="X" class="m-[50px]  rounded-full">`; 
       data[num] = "x"; 
     } else {
-      e.target.innerHTML = `<img src='${circle_img}' alt="O">`; 
+      e.target.innerHTML = `<img src='${circle_img}' alt="O" class="m-[50px] rounded-full">`; 
       data[num] = "o"; 
     }
+    setCount(count + 1);
+    checkWin();
+  }
 
-    setCount(count + 1); 
+  const checkWin = () => {
+    if(data[0]===data[1] && data[1]===data[2] && data[2]!=="")
+    {
+      won(data[2]);
+    }
+    else if(data[3]===data[4] && data[4]===data[5] && data[5]!=="")
+    {
+      won(data[5]);
+    }
+    else if(data[6]===data[7] && data[7]===data[8] && data[8]!=="")
+      {
+        won(data[8]);
+      }
+    else if(data[0]===data[3] && data[3]===data[6] && data[6]!=="")
+      {
+        won(data[6]);
+      }
+    else if(data[1]===data[4] && data[4]===data[7] && data[7]!=="")
+      {
+        won(data[7]);
+      }
+    else if(data[2]===data[5] && data[5]===data[8] && data[8]!=="")
+      {
+        won(data[8]);
+      }
+    else if(data[0]===data[4] && data[4]===data[8] && data[8]!=="")
+      {
+        won(data[8]);
+      }
+    else if(data[0]===data[1] && data[1]===data[2] && data[2]!=="")
+      {
+        won(data[2]);
+      }
+    else if(data[2]===data[4] && data[4]===data[6] && data[6]!=="")
+      {
+        won(data[6]);
+      }     
+  }
+
+  const won = (winner) => {
+    setLock(true);
+    if (titleRef.current) {
+      if (winner === "x") {
+        titleRef.current.innerHTML = `Congratulations: <img src=${cross_img} alt="X" class="p-[20px] h-[80px]"/>`;
+      } else {
+        titleRef.current.innerHTML = `Congratulations: <img src=${circle_img} alt="O" class="p-[20px] h-[90px]"/>`;
+      }
+    }
   }
 
 
