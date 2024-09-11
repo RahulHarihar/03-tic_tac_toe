f4a8be5f981dc6a641692149532b4e6941ac745f
unknown <rahulharihar71@gmail.com>
2024-09-11 16:37:07 +0530

    Board changes

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -69,28 +69,34 @@ const Board = ({titleRef}) => {
     setLock(true);
     if (titleRef.current) {
       if (winner === "x") {
-        titleRef.current.innerHTML = `Congratulations: <img src=${cross_img} alt="X" class="p-[20px] h-[80px]"/>`;
+        titleRef.current.innerHTML = `Congratulations: <img src=${cross_img} alt="X" class="p-[20px] h-[80px]"/> Wins`;
       } else {
-        titleRef.current.innerHTML = `Congratulations: <img src=${circle_img} alt="O" class="p-[20px] h-[90px]"/>`;
+        titleRef.current.innerHTML = `Congratulations: <img src=${circle_img} alt="O" class="p-[20px] h-[90px]" Wins/>`;
       }
     }
+  }
+
+  const reset = () =>  {
+    setLock(false);
+    data = ["", "", "", "", "", "", "", "", "",];
+    titleRef.current.innerHTML = `Tic Tac Toe in React`;
   }
 
 
 
   return (
-    <div className="grid grid-cols-3  h-[600px] w-[564px] mx-auto">
-      <div className="flex h-[180px] w-[180px] bg-[#1f3540]  border-[#0f1b21] rounded-[12px] cursor-pointer" onClick={(e) => {toggle(e,0)}}></div>
-      <div className="flex h-[180px] w-[180px] bg-[#1f3540] border-[#0f1b21] rounded-[12px] cursor-pointer" onClick={(e) => {toggle(e,1)}}></div>
-      <div className="flex h-[180px] w-[180px] bg-[#1f3540] border-[#0f1b21] rounded-[12px] cursor-pointer" onClick={(e) => {toggle(e,2)}}></div>
+    <div className="grid grid-cols-3  h-[500px] w-[564px] mx-auto">
+      <div className="flex h-[160px] w-[175px] bg-[#1f3540]  border-[#0f1b21] rounded-[12px] cursor-pointer" onClick={(e) => {toggle(e,0)}}></div>
+      <div className="flex h-[160px] w-[175px] bg-[#1f3540] border-[#0f1b21] rounded-[12px] cursor-pointer" onClick={(e) => {toggle(e,1)}}></div>
+      <div className="flex h-[160px] w-[175px] bg-[#1f3540] border-[#0f1b21] rounded-[12px] cursor-pointer" onClick={(e) => {toggle(e,2)}}></div>
       
-      <div className="flex h-[180px] w-[180px] bg-[#1f3540] border-[#0f1b21] rounded-[12px] cursor-pointer"onClick={(e) => {toggle(e,3)}}></div>
-      <div className="flex h-[180px] w-[180px] bg-[#1f3540] border-[#0f1b21] rounded-[12px] cursor-pointer"onClick={(e) => {toggle(e,4)}}></div>
-      <div className="flex h-[180px] w-[180px] bg-[#1f3540] border-[#0f1b21] rounded-[12px] cursor-pointer"onClick={(e) => {toggle(e,5)}}></div>
+      <div className="flex h-[160px] w-[175px] bg-[#1f3540] border-[#0f1b21] rounded-[12px] cursor-pointer"onClick={(e) => {toggle(e,3)}}></div>
+      <div className="flex h-[160px] w-[175px] bg-[#1f3540] border-[#0f1b21] rounded-[12px] cursor-pointer"onClick={(e) => {toggle(e,4)}}></div>
+      <div className="flex h-[160px] w-[175px] bg-[#1f3540] border-[#0f1b21] rounded-[12px] cursor-pointer"onClick={(e) => {toggle(e,5)}}></div>
 
-      <div className="flex h-[180px] w-[180px] bg-[#1f3540] border-[#0f1b21] rounded-[12px] cursor-pointer"onClick={(e) => {toggle(e,6)}}></div>
-      <div className="flex h-[180px] w-[180px] bg-[#1f3540] border-[#0f1b21] rounded-[12px] cursor-pointer"onClick={(e) => {toggle(e,7)}}></div>
-      <div className="flex h-[180px] w-[180px] bg-[#1f3540] border-[#0f1b21] rounded-[12px] cursor-pointer"onClick={(e) => {toggle(e,8)}}></div>
+      <div className="flex h-[160px] w-[175px] bg-[#1f3540] border-[#0f1b21] rounded-[12px] cursor-pointer"onClick={(e) => {toggle(e,6)}}></div>
+      <div className="flex h-[160px] w-[175px] bg-[#1f3540] border-[#0f1b21] rounded-[12px] cursor-pointer"onClick={(e) => {toggle(e,7)}}></div>
+      <div className="flex h-[160px] w-[175px] bg-[#1f3540] border-[#0f1b21] rounded-[12px] cursor-pointer"onClick={(e) => {toggle(e,8)}}></div>
     </div>
   );
 }
