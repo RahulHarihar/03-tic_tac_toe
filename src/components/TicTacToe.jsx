31839539ed2c810dd62f80f69e8d4f3dca06f947
unknown <rahulharihar71@gmail.com>
2024-09-11 16:37:32 +0530

    Board changes

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -4,13 +4,19 @@ import Board from './Board';
 const TicTacToe = () => {
   let titleRef = useRef(null);
 
+  const reset = () =>  {
+    setLock(false);
+    data = ["", "", "", "", "", "", "", "", "",];
+    titleRef.current.innerHTML = `Tic Tac Toe in React`;  
+  }
+
   return (
     <div className="text-center">
       <h1 ref={titleRef} className="flex justify-center items-center mt-6 px-6 py-6 font-mono font-semibold text-6xl text-white">
         03 - Tic Tac Toe Game
       </h1>
       <Board titleRef={titleRef} />
-      <button className="border bg-blue-400 text-white px-4 py-2 mt-4 rounded">Reset</button>
+      <button onClick={() => {reset()}} className="border bg-blue-400 text-white px-4 py-2 mt-4 rounded">Reset</button>
     </div>
   );
 }
